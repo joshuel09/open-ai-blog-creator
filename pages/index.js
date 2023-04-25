0fe219ad0db085494bdba10d93c7805ff6febda9
joshuel09 <46719153+joshuel09@users.noreply.github.com>
2023-04-25 18:23:42 +0900

    set-up UI/UX
    in this commit the simple UI/UX of the app has been implemented.

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,23 @@
-import Link from "next/link";
-import { useUser } from "@auth0/nextjs-auth0/client"
 import Image from "next/image";
+import HeroImage from "../public/hero.webp"
+import { Logo } from "../components/logo";
+import Link from "next/link";
 
 export default function Home() {
-  const { user } = useUser();
-
-  console.log("User:", user)
   return (
 
-    <div>
-      <h1>this is home page</h1>
-      <div>{
-        !!user ? <>
-          <div>
-            <Image
-              src={user.picture}
-              alt={user.name}
-              height={50}
-              width={50}
-            />
-            <div>{user.email}</div>
-          </div>
-          <Link href="api/auth/logout">Logout</Link>
-        </> :
-          <Link href="api/auth/login">Login</Link>
-      }
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image src={HeroImage} alt="Hero" fill className="absolute" />
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
+        <Logo />
+        <p>
+          Revolutionize your content creation game with our cutting-edge AI-powered SAAS solution!
+          Say goodbye to endless hours spent on crafting SEO-optimized blog posts, and hello to high-quality content in just minutes.
+          Get ready to take your business to new heights with our game-changing solution.
+        </p>
+        <Link href="/post/new" className="btn mt-2">
+          Let's Begin
+        </Link>
       </div>
     </div>
 
--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,9 +1,19 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../components/AppLayout";
 
 export default function TokenTopup() {
-    return <div>this is token top up page</div>;
-  }
-  
+    return (
+        <div>
+            this is token top up page
+        </div>
+    );
+}
+
+
+TokenTopup.getLayout = function getLayout(page, pageProps) {
+
+    return <AppLayout {...pageProps}>{page}</AppLayout>
+}
 
 export const getServerSideProps = withPageAuthRequired(() => {
     return {
