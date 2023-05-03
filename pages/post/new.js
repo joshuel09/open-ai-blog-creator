7fef68fb567cf814f9095772746c8e7bb77f4c5c
Josusama™ <46719153+joshuel09@users.noreply.github.com>
2023-05-04 01:01:46 +0900

    quick fix

--- pages/post/new.js
+++ pages/post/new.js
@@ -2,6 +2,7 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { useState } from "react";
 import { AppLayout } from "../../components/AppLayout";
 import { useRouter } from "next/router";
+import { getAppProps } from "../../utils/getAppProps";
 
 export default function NewPost(props) {
     const router = useRouter();
